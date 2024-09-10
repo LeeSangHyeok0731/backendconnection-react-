3aeba65bfa23927c98d33f80a4541a959fef6fcd
fakesanghyeok <s24066@gsm.hs.kr>
2024-09-10 22:55:31 +0900

    공지글 지우기2

--- src/notion.js
+++ src/notion.js
@@ -13,7 +13,6 @@ const WriteJova = styled.div`
 
 const P = styled.p`
     margin: 0;
-    src=""
     grid-column: 1/10;
     grid-row: 2/3;
 `;
@@ -53,19 +52,36 @@ const WritttenNotion = styled.div`
     grid-template-rows: repeat(2, 25px);
 `;
 
-const Delete = styled.img`
-    border:1px solid red;
+const Delete = styled.div`
     width:50px;
     height:25px;
     grid-column: 10/11;
     grid-row: 1/2;
-    
+
 `
 
 const TitleText = styled.span`
-    grid-column:1/10;
+    grid-column:1/11;
     grid-row:1/2;
 `
+
+const DeleteAlert = styled.div`
+    display:block;
+    posiiton: absolution;
+    backgroun-color:red;
+`
+
+const MySvgIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      aria-hidden="true"
+      style={{ pointerEvents: 'none', display: 'inherit', width: '100%', height: '100%' }}
+    >
+      <path d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5M10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5m0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5" />
+    </svg>
+  );
 
 function Notion() {
     const [isWritePageVisible, setWritePageVisible] = useState(false);
@@ -80,6 +96,10 @@ function Notion() {
         OnWritePage();
     };
 
+    const DeleteNotion = (keyNumber) => {
+        setNotions((prevNotions) => prevNotions.filter((_, i) => i !== index));
+    }
+
     return (
         <WriteJova>
             <WritePage show={isWritePageVisible}>
@@ -93,7 +113,9 @@ function Notion() {
                 <WritttenNotion key={index}>
                     <TitleText>{notion.title}</TitleText>
                     <P>{notion.majors.join(", ")}</P>
-                    <Delete />
+                    <Delete onClick={() => DeleteNotion(index)}>
+                        <MySvgIcon />
+                    </Delete>
                 </WritttenNotion>
             ))}
         </WriteJova>
